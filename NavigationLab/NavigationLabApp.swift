8ae603c3d11b7cf1a6aedf2b33fbeae4fb2705dc
PresnoArmweak <preston.armstrong1234@gmail.com>
2026-04-15 09:09:49 -0500

    Initial Commit

---
+++ NavigationLab/NavigationLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLabApp.swift
+//  NavigationLab
+//
+//  Created by ARMSTRONG, PRESTON on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
