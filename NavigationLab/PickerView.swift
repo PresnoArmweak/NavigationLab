010668fbe2d835bb5243380bb005bb641de31dd8
vg0996425 <wg0996425@otc.edu>
2026-04-15 09:39:51 -0500

    initial commitio

---
+++ NavigationLab/PickerView.swift
@@ -0,0 +1,37 @@
+//
+//  PickerView.swift
+//  NavigationLab
+//
+//  Created by GRAY, VIKTOR on 4/15/26.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    @Binding var song: Song
+        @Environment(\.dismiss) var dismiss
+
+        var body: some View {
+            List(Song.allCases) { songOption in
+                Button {
+                    song = songOption
+                    dismiss()
+                } label: {
+                    HStack {
+                        Image(systemName: songOption.symbol)
+                            .font(.largeTitle)
+                        Text(songOption.rawValue.capitalized)
+                            .font(.headline)
+                            .padding(.leading)
+                    }
+                }
+            }
+            .navigationTitle("Pick Your Mood")
+        }
+    }
+
+#Preview {
+    NavigationStack {
+        PickerView(song: .constant(.father))
+    }
+}
